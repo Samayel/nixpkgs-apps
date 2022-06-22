42c5e7188c55117774f811af4f25ef439e314329
Markus John <johm@quyo.de>
2022-06-23 00:16:28 +0200

    Add nix-pills 16

---
+++ nix-pills/16-repository/default.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+  allPkgs = nixpkgs // pkgs;
+  callPackage = path: overrides:
+    let
+      f = import path;
+      lib = import ./lib.nix;
+    in lib.makeOverridable f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+  pkgs = with nixpkgs; rec {
+    mkDerivation = import ./autotools.nix nixpkgs;
+    hello = callPackage ./hello.nix { };
+    graphviz = callPackage ./graphviz.nix { };
+    graphvizMutant = graphviz.override { gd = hello; };
+  };
+in pkgs
