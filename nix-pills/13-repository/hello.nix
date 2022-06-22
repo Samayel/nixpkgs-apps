84db863a21aa1f2bae689602f2b8dcec6b39b239
Markus John <johm@quyo.de>
2022-06-22 19:53:55 +0200

    Add nix-pills 13, rename nix-pill 12

---
+++ nix-pills/13-repository/hello.nix
@@ -0,0 +1,9 @@
+{ mkDerivation }:
+
+mkDerivation {
+  name = "hello";
+  src = builtins.fetchurl {
+    url = "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz";
+    sha256 = "8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20";
+  };
+}
