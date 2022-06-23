7d30cbaa311461951ee3197c64d572219f8534f0
Markus John <johm@quyo.de>
2022-06-23 21:13:58 +0200

    Fix ecmgit

--- mersenneforum/packages/ecmgit.nix
+++ mersenneforum/packages/ecmgit.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, gmp, m4 }:
+{ lib, stdenv, fetchurl, autoreconfHook, gmp, m4 }:
 
 let
   pname = "ecm";
@@ -14,12 +14,17 @@ stdenv.mkDerivation {
     rev = "5663e00cb4880a6ee6393eb9067e9eea201098d4";
   };
 
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+
   # See https://trac.sagemath.org/ticket/19233
   configureFlags = lib.optional stdenv.isDarwin "--disable-asm-redc";
 
   buildInputs = [ m4 gmp ];
 
   doCheck = true;
+  preCheck = "sed -i -e 's|^/bin/rm |rm |g' test.*";
 
   meta = {
     description = "Elliptic Curve Method for Integer Factorization";
