3083cc1dedc14610beef9a9fdac1c76830f57aa9
Markus John <johm@quyo.de>
2022-10-12 17:49:34 +0200

    Update table.inline-fakechannel.py

--- scripts/table.inline-fakechannel.py
+++ scripts/table.inline-fakechannel.py
@@ -1,9 +1,6 @@
-#! /usr/bin/env -S nix run github:Samayel/qnixpkgs#nix-fakechannel -- github:nixos/nixpkgs/release-22.05 nix shell github:nixos/nixpkgs/release-22.05#nix -c nix-shell
+#! /usr/bin/env -S nix run github:Samayel/qnixpkgs#nix-fakechannel -- nix shell github:nixos/nixpkgs/release-22.05#nix -c nix-shell
 #! nix-shell --pure -i python3
 #! nix-shell -p "python3.withPackages (ps: [ ps.prettytable ])"
-
-#                         [nix-fakechannel source ]                  [fake nixpkgs channel => python3 src]        [nix-shell source flake            ]
-#  /usr/bin/env -S nix run github:Samayel/qnixpkgs#nix-fakechannel -- github:nixos/nixpkgs/release-22.05 nix shell github:nixos/nixpkgs/release-22.05#nix -c nix-shell
 
 import prettytable
 
